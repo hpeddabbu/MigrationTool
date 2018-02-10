92d501482eb7cbafb345b3340bf043d99c8c9e06
Harini <harini5693@gmail.com>
2018-02-10 00:23:10 -0500

    Added code to read data from mysql and write to mongodb

--- ApplicationStater.py
+++ ApplicationStater.py
@@ -4,7 +4,8 @@ import sys
 from Migration.ConnectionAdaptor import MysqlConnectionAdaptor
 from Migration.MetadataAdaptor import MysqlMetadataAdaptor
 from Migration.CoreAdaptor import MysqlCoreAdaptor
-
+from Migration.NosqlConnectionAdaptor import ConnectionMongodb
+from Migration.NosqlMetadaAdaptoe import MetaMongodb
 
 def main(argv):
 
@@ -22,7 +23,12 @@ def main(argv):
     core_obj = MysqlCoreAdaptor(session, table_name, field_info)
     core_obj.output_data()
 
+    con_obj = ConnectionMongodb()
+    con_obj.init_connection_mongo()
+    mongo_ses = con_obj.get_mongo_session
 
+    meta_obj = MetaMongodb(mongo_ses, session,table_name, field_info)
+    meta_obj.retrieve_metadata_mongodb()
 
 
 if __name__ == '__main__':
--- Migration/NosqlMetadaAdaptoe.py
+++ Migration/NosqlMetadaAdaptoe.py
@@ -2,8 +2,12 @@ class MetaMongodb(object):
     """
     This class is for getting meta data from the database
     """
-    def __init__(self, mongo_session):
+
+    def __init__(self, mongo_session,session,table_name, field_info):
         self.mongo_session = mongo_session
+        self.session = session
+        self.table_name = table_name
+        self.field_info = field_info
         self.collection_name = None
         self.metaField = None
 
@@ -15,8 +19,60 @@ class MetaMongodb(object):
         #print(db)
         # viewing database
 
-        db = self.mongo_session['local']
-        print(db)
+
+
+        cur = self.session.cursor()
+
+        db_mongo = self.mongo_session['local']
+        print(db_mongo)
+
+        fields = ','.join(str(s) for s in self.field_info)
+        print(fields)
+        cur.execute('select ' + fields + ' from ' + self.table_name)
+        row_titles = self.field_info
+
+        # mongo client specifically requires python dict
+        cus = dict()
+
+        # custom record id rather than mongodb default hash id
+        cid = 0
+
+        # cycle through each mySQL row
+        for (row) in cur:
+            cid += 1  # increment id
+            cus['_id'] = cid
+
+            # check if current row is null
+            for i in range(0, len(row)):
+                if row[i] == None:
+                    # if the record is null, skip it
+                    continue
+                else:
+                    # conversion to string
+                    row_title = "".join(row_titles[i])
+                    # conversion to string
+                    field = str(row[i])
+
+                    # add current record's field's title and value
+                    cus[row_title] = field
+
+            # we've completed processing this row, insert it into mongoldb
+            id = db_mongo.collection11.insert_one(cus).inserted_id
+
+        cn = db_mongo.collection_names()
+        for c in cn:
+            print(c)
+
+        col = input("Please select the collection name")
+
+        csr = db_mongo.collection11.find({})
+        for document in csr:
+            print(document)
+            # just for debug purposes, show me running row count
+            #print(id)
+
+            # shut down the mysql connection
+
 
         # to know what databases are available
         # db_names = db.database_names()
@@ -24,19 +80,16 @@ class MetaMongodb(object):
         # to view the collections
         # collection = db.my_collection
         # print(collection)
-        cn = db.collection_names()
-        for c in cn:
-            print(c)
 
-        col = input("Please select the collection name")
         # Each document in the files collection represents a file in GridFS.
         # to get the meta data
-        collection = db[col].find()
+        """collection = db_mongo[col].find().pretty()
         print(collection)
         for item in collection:
             # print(item)
             for key in item.keys():
-                print(key)
+                print(key)"""
+        cur.close()
 
     @property
     def get_collection_name(self):
